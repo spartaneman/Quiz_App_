1824390c61c7ed64e58046b6856cdd3c4b10ad7b
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-12 13:37:21 -0800

    Building the textview button function. First the UI

--- app/src/main/java/com/example/quiz_app_/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quiz_app_/QuizQuestionActivity.kt
@@ -1,5 +1,7 @@
 package com.example.quiz_app_
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,11 +9,13 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 //adding the View.OnClickListener to allow selection of textview
 //Must implement override of onClick
 class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
 
+    //Control values of the current questions and questions
     private var mCurrentPosition: Int = 0
     private var mQuestionsList: ArrayList<Question>? = null
     private var mSelectedAnswer: Int = 0
@@ -45,12 +49,51 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
 
         mQuestionsList = Constants.getQuestions()
         setQuestion()
+        defaultAnswersView()
     }
 
     override fun onClick(p0: View?) {
         TODO("Not yet implemented")
     }
 
+    //This function creates the default look of the textview buttons.
+    private fun defaultAnswersView()
+    {
+        val answers = ArrayList<TextView>()
+        //To add the textviews into the arrayList we must use the let
+        //since the variables are null to begin with
+        tvAnswerZero?.let {
+            answers.add(0,it)
+        }
+        tvAnswerOne?.let {
+            answers.add(1,it)
+        }
+        tvAnswerTwo?.let {
+            answers.add(2,it)
+        }
+        tvAnswerThree?.let {
+            answers.add(3,it)
+        }
+        tvAnswerFour?.let {
+            answers.add(4,it)
+        }
+
+        for(answer in answers)
+        {
+            //This will be the default options
+            answer.setTextColor(Color.parseColor("#8A2308"))
+            answer.typeface = Typeface.DEFAULT
+            answer.background = ContextCompat.getDrawable(this,R.drawable.answer_textview)
+        }
+    }
+
+    //This function will change the view of the selected textview
+    private fun selectedAnswerView(tview: TextView)
+    {
+
+    }
+
+    //this will set the text and images according to the current position in the question ArrayList
     private fun setQuestion()
     {
         //!! is the not-null operator that converts any value to a non-null type.
@@ -66,5 +109,12 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
         tvAnswerTwo?.text = question.optionTwo
         tvAnswerThree?.text = question.optionThree
         tvAnswerFour?.text = question.optionFour
+
+        if(mCurrentPosition == mQuestionsList!!.size) {
+            btnSubmit?.text = "Finish"
+        }else{
+            btnSubmit?.text = "Submit"
+        }
+
     }
 }
