020afc8166a92965c674edb3275a0584b29fd1ba
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-11 19:21:20 -0800

    created variables for the textview in the quiz question activity

--- app/src/main/java/com/example/quiz_app_/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/quiz_app_/QuizQuestionActivity.kt
@@ -2,10 +2,36 @@ package com.example.quiz_app_
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionActivity : AppCompatActivity() {
+    private var progressBar: ProgressBar? = null
+    private var tvProgressbar: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+
+    private var tvAnswerZero: TextView? = null
+    private var tvAnswerOne: TextView? = null
+    private var tvAnswerTwo: TextView? = null
+    private var tvAnswerThree: TextView? = null
+    private var tvAnswerFour: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        progressBar = findViewById(R.id.progressBar)
+        tvProgressbar = findViewById(R.id.tvProgress)
+        tvQuestion = findViewById(R.id.tvQuestion)
+        ivImage = findViewById(R.id.ivFlag)
+        tvAnswerZero = findViewById(R.id.tvOptionZero)
+        tvAnswerOne = findViewById(R.id.tvOptionOne)
+        tvAnswerTwo = findViewById(R.id.tvOptionTwo)
+        tvAnswerThree = findViewById(R.id.tvOptionThree)
+        tvAnswerFour = findViewById(R.id.tvOptionFour)
+
+
     }
 }
