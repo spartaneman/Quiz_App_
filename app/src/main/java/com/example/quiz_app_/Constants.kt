2486191b88f9df8fbd67b5b639d58bca40c81ff2
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-01-11 16:34:34 -0800

    Added variables to data class and created object Constants

---
+++ app/src/main/java/com/example/quiz_app_/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.quiz_app_
+
+object Constants {
+}
--- app/src/main/java/com/example/quiz_app_/Question.kt
+++ app/src/main/java/com/example/quiz_app_/Question.kt
@@ -1,3 +1,13 @@
 package com.example.quiz_app_
 
-data class Question()
+data class Question(
+    val id: Int,
+    val questions: String,
+    val image: Int,
+    val optionZero:String,
+    val optionOne:String,
+    val optionTwo:String,
+    val optionThree:String,
+    val optionFour:String,
+    val correctAnswer: Int,
+)
